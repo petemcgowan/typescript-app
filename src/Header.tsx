664f734fa7238eebed6b725844ecf98886f4ccf7
Pete <peteburquette@gmail.com>
2021-09-15 00:55:07 +0100

    adding basic typescript component

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,16 +1,24 @@
 import React, {useState} from 'react'
 
+// https://www.sitepoint.com/react-with-typescript-best-practices/
+
 interface HeaderProps {
-  buttonText: string;
+  buttonText?: string;   // ? means it's not a required property
 }
 
+interface Movie {
+  title: string;
+  date: string;
+  rating: string;
+  description: string;
+}
 
 function Header(props: HeaderProps) {
-  // const [count, setCount] = useState<number | null>(0);
-  const [count, setCount] = useState<number>(0);
+  const [count, setCount] = useState<number | null>(0);
+  const [movie, setMovie] = useState<Movie | null>(null);
 
   const increment = () => {
-    setCount(count + 1);
+    setCount(count! + 1);  // ! indicates that I know this is not null
   }
 
   return (
